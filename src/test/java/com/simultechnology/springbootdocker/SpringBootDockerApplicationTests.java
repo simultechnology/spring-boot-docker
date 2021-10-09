826e2952bfbdd5b7f3230ffd6df0ab436cc67b50
t_ishikawa <tissi0708@gmail.com>
2021-10-09 21:49:46 +0900

    Initial commit

---
+++ src/test/java/com/simultechnology/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simultechnology.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
